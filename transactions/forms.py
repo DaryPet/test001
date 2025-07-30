a12a45755d3a1cae37e96d114ae0bb5b25d4c828
DaryPet <d7akkord@gmail.com>
2025-07-30 17:26:27 +0200

    refactoring

--- transactions/forms.py
+++ transactions/forms.py
@@ -3,6 +3,7 @@ Django forms for the transactions application.
 """
 from django import forms
 from .models import Transaction
+from decimal import Decimal
 
 class TransactionForm(forms.ModelForm):
     """
@@ -16,7 +17,16 @@ class TransactionForm(forms.ModelForm):
         fields = ['type', 'amount']
         widgets = {
             'amount': forms.NumberInput(
-                attrs={'class': 'form-control', 'step': '0.01', 'min': '0.01'}
+                attrs={'class': 'form-control', 'step': '0.01', 'min': '0.01', 'id': 'amountInput'}
                 ),
-            'type': forms.Select(attrs={'class': 'form-select'}),
+            'type': forms.Select(attrs={'class': 'form-select', 'id': 'typeSelect'}),
         }
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount is None:
+            raise forms.ValidationError("Please, eneter amount")
+        if amount <= Decimal('0.00'):
+            raise forms.ValidationError('Amount should be more then 0')  
+        return amount
+
